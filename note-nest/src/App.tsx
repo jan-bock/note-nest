fb4adf95265cccfb9f25ea250a83a16733f86ea5
Jan Bock <113291832+jan-bock@users.noreply.github.com>
2024-03-20 22:52:12 +0000

    component: staging NotesPlayGround

--- note-nest/src/App.tsx
+++ note-nest/src/App.tsx
@@ -2,12 +2,14 @@ import './App.css'
 
 import { ChakraProvider, Divider } from '@chakra-ui/react'
 import { NavBar } from './components/NavBar/NavBar'
+import { NotesPlayGround } from './components/NotesPlayGround/NotesPlayGround'
 
 const App = () => {
   return (
     <ChakraProvider>
       <NavBar />
       <Divider />
+      <NotesPlayGround />
     </ChakraProvider>
   )
 }
